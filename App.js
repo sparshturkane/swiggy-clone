3638fe4b30ff24d6098f8929be4161f616ef639e
sparshturkane <rajaturkane99@gmail.com>
2024-03-30 22:24:00 +0530

    tailwind is working, added react-navigation

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen() {
   return (
-    // <View style={styles.container}>
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Hey this works</Text>
-      <StatusBar style="auto" />
+    // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-500">Home Screen</Text>
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+
+const Stack = createNativeStackNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
