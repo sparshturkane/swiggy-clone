fd222aebf2cef7d2243174dcf1fa8b89990163f8
sparshturkane <rajaturkane99@gmail.com>
2024-03-30 22:49:00 +0530

    Closing DAY 1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,14 @@
-import { Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { useNavigation } from '@react-navigation/native';
 
 function HomeScreen() {
+    const navigation = useNavigation();
     return (
-        <View className="flex-1 items-center justify-center">
+        <SafeAreaView className="flex-1 items-center justify-center">
             <Text className="text-red-500">Home Screen</Text>
             <StatusBar style="auto" />
-        </View>
+        </SafeAreaView>
     );
 }
 
